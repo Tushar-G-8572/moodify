9f54d0bd2b454b7b6eadd9288184973f4d3b41a3
Tushar Gupta <tusharg.mp1@gamil.com>
2026-03-05 15:54:24 +0530

    Frontend Integration Complete

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,10 +4,12 @@ const cors = require('cors');
 const authRouter = require('./routes/auth.routes');
 const userRouter = require("./routes/user.routes");
 const songRouter = require('./routes/song.routes');
+const path = require('path'); 
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(express.static("./public"))
 app.use(cors({
     origin:"http://localhost:5173",
     credentials:true,
@@ -19,6 +21,10 @@ app.use('/api/auth',authRouter);
 app.use('/api/user',userRouter);
 
 app.use('/api/song',songRouter);
+
+app.use("*name",(req,res)=>{
+    res.sendFile(path.join(__dirname,"..","/public/index.html"));
+})
 
 
 module.exports = app;
