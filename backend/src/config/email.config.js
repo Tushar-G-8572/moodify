13819eb23bf2b75a9614848cc444b0b6ba999cb1
Tushar Gupta <tusharg.mp1@gamil.com>
2026-03-06 12:22:10 +0530

    fixing error

--- backend/src/config/email.config.js
+++ backend/src/config/email.config.js
@@ -1,31 +1,31 @@
 const nodemailer = require('nodemailer');
 
-const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    family: 4,
-    auth:{
-        type:"OAuth2",
-        user: process.env.EMAIL_USER,
-        clientId: process.env.CLIENT_ID,
-        clientSecret: process.env.CLIENT_SECRET,
-        refreshToken: process.env.REFRESH_TOKEN
-    },
-    connectionTimeout:20000
-})
-
 // const transporter = nodemailer.createTransport({
-//   host: "smtp.example.com",
-//   port: 587,
-//   secure: false,
-//   family: 4, 
-//   auth: {
-//     user: process.env.EMAIL_USER,
-//     pass: process.env.EMAIL_PASS
-//   },
-//   connectionTimeout: 20000
-// });
+//     host: "smtp.gmail.com",
+//     port: 465,
+//     secure: true,
+//     family: 4,
+//     auth:{
+//         type:"OAuth2",
+//         user: process.env.EMAIL_USER,
+//         clientId: process.env.CLIENT_ID,
+//         clientSecret: process.env.CLIENT_SECRET,
+//         refreshToken: process.env.REFRESH_TOKEN
+//     },
+//     connectionTimeout:20000
+// })
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  family: 4, 
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  },
+  connectionTimeout: 20000
+});
 
 transporter.verify((error, success) => {
   if (error) {
